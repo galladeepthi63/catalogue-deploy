cba1f60913d0a01189c9254fe961e9b890d4b22f
galladeepthi63 <deepthig63@outlook.com>
2024-02-05 14:26:31 +0530

    catalague jenkins

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,15 +2,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "5.31.0" # AWS provider version, not terraform version
     }
   }
+
+  backend "s3" {
+    bucket         = "erraformjenk"
+    key            = "catalogue"
+    region         = "us-east-1"
+    dynamodb_table = "daws76s-locking-dev"
+  }
 }
-
-
-
-# Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
+
 
